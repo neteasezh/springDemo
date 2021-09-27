c398c7c7163bb52d90c2e16777361bfa9593fb63
mrzhu <1422554131@qq.com>
2021-09-27 16:54:03 +0800

    Netty学习1

--- src/main/java/com/wust/git/TestGit.java
+++ src/main/java/com/wust/git/TestGit.java
@@ -3,5 +3,7 @@ package com.wust.git;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("hello,git");
+        System.out.println("hello,git1");
+        System.out.println("hello,git2");
     }
 }
