b901fecdc6e6bde2ec4c0988aa8c1d7b51b8644c
wustzh <49762189+wustzh@users.noreply.github.com>
2021-09-27 17:12:40 +0800

    Update TestGit.java

--- src/main/java/com/wust/git/TestGit.java
+++ src/main/java/com/wust/git/TestGit.java
@@ -7,5 +7,6 @@ public class TestGit {
         System.out.println("hello,git2");
         System.out.println("hello,git3");
         System.out.println("hello,master");
+        System.out.println("hello,hot-fix");
     }
 }
